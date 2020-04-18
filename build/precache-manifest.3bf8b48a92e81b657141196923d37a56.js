1e89c75d4f36669a53cbcaf6f2cc20b3d8039c41
Vikrant Kumar <vikrantkr1211@gmail.com>
2020-04-19 03:49:39 +0530

    Add files via upload

---
+++ build/precache-manifest.3bf8b48a92e81b657141196923d37a56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84ae376c589b33688216f6dd3001729",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1052dc2281f72ae59683",
+    "url": "/static/css/main.372ee532.chunk.css"
+  },
+  {
+    "revision": "883fea906ac2f6d0a0fc",
+    "url": "/static/js/2.70693479.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.70693479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1052dc2281f72ae59683",
+    "url": "/static/js/main.709d41e9.chunk.js"
+  },
+  {
+    "revision": "6909ddb41416de7848ac",
+    "url": "/static/js/runtime-main.dac0035b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
